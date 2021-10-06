7b05708667843bd28a6993a875102642a4fad166
Andrew Krasavin <darkseed2007@yandex.ru>
2021-10-06 06:01:56 +0300

    Update abseil to 20210324.2

--- abseil/patches/patch-absl_debugging_failure__signal__handler.cc
+++ abseil/patches/patch-absl_debugging_failure__signal__handler.cc
@@ -1,11 +1,11 @@
---- absl/debugging/failure_signal_handler.cc.orig	Thu May 13 00:14:05 2021
-+++ absl/debugging/failure_signal_handler.cc	Thu May 13 00:14:12 2021
-@@ -136,7 +136,7 @@
- #else
+--- absl/debugging/failure_signal_handler.cc.orig	Tue Jun  1 17:52:49 2021
++++ absl/debugging/failure_signal_handler.cc	Wed Oct  6 05:46:01 2021
+@@ -137,7 +137,7 @@
    const size_t page_mask = sysconf(_SC_PAGESIZE) - 1;
  #endif
--  size_t stack_size = (std::max(SIGSTKSZ, 65536) + page_mask) & ~page_mask;
-+  size_t stack_size = (std::max(SIGSTKSZ, (unsigned int)65536) + page_mask) & ~page_mask;
+   size_t stack_size =
+-      (std::max<size_t>(SIGSTKSZ, 65536) + page_mask) & ~page_mask;
++      (std::max<size_t>(SIGSTKSZ, (unsigned int)65536) + page_mask) & ~page_mask;
  #if defined(ABSL_HAVE_ADDRESS_SANITIZER) || \
      defined(ABSL_HAVE_MEMORY_SANITIZER) || defined(ABSL_HAVE_THREAD_SANITIZER)
    // Account for sanitizer instrumentation requiring additional stack space.
