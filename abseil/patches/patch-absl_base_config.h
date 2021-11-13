9bc71bf3451eda083ea75a0034b18062fc708276
Andrew Krasavin <noiseless-ak@yandex.ru>
2021-11-13 04:07:15 +0300

    Update abseil to 20211102.0

--- abseil/patches/patch-absl_base_config.h
+++ abseil/patches/patch-absl_base_config.h
@@ -1,19 +1,20 @@
---- absl/base/config.h.orig	Wed May 12 23:51:28 2021
-+++ absl/base/config.h	Wed May 12 23:58:31 2021
-@@ -370,6 +370,7 @@
- #ifdef ABSL_HAVE_MMAP
- #error ABSL_HAVE_MMAP cannot be directly set
- #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) ||   \
-+    defined(__OpenBSD__) || \
-     defined(__ros__) || defined(__native_client__) || defined(__asmjs__) || \
-     defined(__wasm__) || defined(__Fuchsia__) || defined(__sun) || \
-     defined(__ASYLO__) || defined(__myriad2__)
-@@ -383,7 +384,7 @@
+--- absl/base/config.h.orig	Wed Nov  3 18:26:14 2021
++++ absl/base/config.h	Sat Nov 13 02:26:25 2021
+@@ -414,7 +414,7 @@
+     defined(_AIX) || defined(__ros__) || defined(__native_client__) ||    \
+     defined(__asmjs__) || defined(__wasm__) || defined(__Fuchsia__) ||    \
+     defined(__sun) || defined(__ASYLO__) || defined(__myriad2__) ||       \
+-    defined(__HAIKU__)
++    defined(__HAIKU__) || defined(__OpenBSD__)
+ #define ABSL_HAVE_MMAP 1
+ #endif
+ 
+@@ -425,7 +425,7 @@
  #ifdef ABSL_HAVE_PTHREAD_GETSCHEDPARAM
  #error ABSL_HAVE_PTHREAD_GETSCHEDPARAM cannot be directly set
  #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || \
--    defined(__ros__)
-+    defined(__ros__) || defined(__OpenBSD__)
+-    defined(_AIX) || defined(__ros__)
++    defined(_AIX) || defined(__ros__) || defined(__OpenBSD__)
  #define ABSL_HAVE_PTHREAD_GETSCHEDPARAM 1
  #endif
  
